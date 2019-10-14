a28da4b3776399e3ce2066f5b6a4284776e74976
Eric Jennings <erjennings13@gmail.com>
2019-10-13 19:52:14 -0700

    fileio except savepatrons

--- library/fileIO.cpp
+++ library/fileIO.cpp
@@ -2,11 +2,8 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
-#include <string>
 #include <vector>
-#include <list>
-#include <stdlib.h>
-#include <ctype.h>
+
 using namespace std;
 /* clears, then loads books from the file filename
  * returns  COULD_NOT_OPEN_FILE if cannot open filename
@@ -26,6 +23,33 @@ int loadBooks(std::vector<book> &books, const char* filename)
 		return NO_BOOKS_IN_LIBRARY;
 	}
 	else {
+		std::string line;
+		std::string token;
+		std::string token2;
+		std::string token3;
+		book myBook;
+		stringstream ss;
+
+		while (getline(myfile, line)) {
+			book myBook;
+			ss.str(line);
+
+			getline(ss, token, ',');
+			myBook.book_id = stoi(token);
+
+			getline(ss, myBook.title, ',');
+
+			getline(ss, myBook.author, ',');
+
+			getline(ss, token2, ',');
+			myBook.state = static_cast<book_checkout_state>(stoi(token2));
+
+			getline(ss, token3, ',');
+			myBook.loaned_to_patron_id = stoi(token3);
+
+			books.push_back(myBook);
+			ss.clear();
+		}
 
 	}
 	return SUCCESS;
@@ -38,6 +62,28 @@ int loadBooks(std::vector<book> &books, const char* filename)
  * */
 int saveBooks(std::vector<book> &books, const char* filename)
 {
+	ofstream myFile;
+	myFile.open(filename);
+
+	if (!myFile.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	}
+	else if (books.empty()){
+		return NO_BOOKS_IN_LIBRARY;
+	}
+	else{
+		string myData;
+
+		for (int var = 0; var < books.size(); ++var) {
+			myData = to_string(books[var].book_id) + "," + books[var].title + "," + books[var].author + "," + to_string(books[var].state) + "," + to_string(books[var].loaned_to_patron_id);
+			myFile<<myData<<std::endl;
+		}
+		if (myFile.is_open()) {
+			myFile.close();
+		}
+	}
+
+
 	return SUCCESS;
 }
 
@@ -48,6 +94,41 @@ int saveBooks(std::vector<book> &books, const char* filename)
  * */
 int loadPatrons(std::vector<patron> &patrons, const char* filename)
 {
+	patrons.clear();
+
+	ifstream myfile;
+	myfile.open(filename);
+	if(!myfile.is_open()) {
+		return COULD_NOT_OPEN_FILE;
+	}
+	else if (myfile.peek() == std::ifstream::traits_type::eof()) {
+		return NO_PATRONS_IN_LIBRARY;
+	}
+	else {
+		std::string line;
+		std::string token;
+		std::string token2;
+		patron myPatron;
+		stringstream ss;
+
+		while (getline(myfile, line)) {
+			patron myPatron;
+			ss.str(line);
+
+			getline(ss, token, ',');
+			myPatron.patron_id = stoi(token);
+
+			getline(ss, myPatron.name, ',');
+
+			getline(ss, token2, ',');
+			myPatron.number_books_checked_out = stoi(token2);
+
+			patrons.push_back(myPatron);
+			ss.clear();
+		}
+
+	}
+
 	return SUCCESS;
 }
 
